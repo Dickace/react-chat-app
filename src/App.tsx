9ce9140027967de1d454e0c59f995f7323930a30
Roman <romix0000@gmail.com>
2021-10-12 02:09:53 +0700

    feat: check prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-const SomeComponent = () =>{
-        const some = 2;
-  return (
-            <div>Some</div>
-  )
+const SomeComponent = () => {
+        const some = 2
+
+
+  return <div>Some</div>
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  return (<div className="App">
+               <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -27,7 +26,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
